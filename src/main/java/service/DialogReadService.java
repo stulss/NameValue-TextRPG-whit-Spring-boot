b1aa09ab64c0b1d6d2832da2ccc37824ac399fce
forktheonlyone <forktheonlyone@gmail.com>
2023-10-06 10:40:28 +0900

    다이얼로그 게임 웨폰 서비스 수정

--- src/main/java/service/DialogReadService.java
+++ src/main/java/service/DialogReadService.java
@@ -7,16 +7,111 @@ import java.util.Map;
 
 public class DialogReadService {
 
-    public static String getDialog(String key) {
+    private String getDialog(String key) {
         ObjectMapper mapper = new ObjectMapper();
 
         try {
             Map<String, String> dialogMap =
-                    mapper.readValue(Paths.get("./Dialog.json").toFile(), Map.class);
+                    mapper.readValue(Paths.get("../Dialog.json").toFile(), Map.class);
             return dialogMap.get(key);
         } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
     }
+
+    private void printDialog(String dialogKey) {
+        String dialog = DialogReadService.getDialog(dialogKey);
+
+        if (dialog != null) {
+            System.out.println(dialog);
+        }
+        else {
+            System.out.println("해당 키에 대한 다이얼로그가 JSON 파일에 없습니다: " + dialogKey);
+        }
+    }
+
+    public void titleDialog() { String dialogKey = "TitleDialog"; }
+
+    public void inputName() {
+        String dialogKey = "InputName";
+    }
+
+    public void wrongDialog() {
+        String dialogKey = "WrongDialog";
+    }
+    public void selectMenuDialog() {
+        String dialogKey = "SelectMenuDialog";
+    }
+    public void errorDelayDialog() {
+        String dialogKey = "ErrorDelayDialog";
+    }
+    public void gameStoryDialog() {
+        String dialogKey = "GameStoryDialog";
+    }
+    public void gameStoryMenu() {
+        String dialogKey = "GameStoryMenu";
+    }
+    public void poorDialog() {
+        String dialogKey = "PoorDialog";
+    }
+    public void villageBackGround() {
+        String dialogKey = "VillageBackGround";
+    }
+    public void villageMenuDialog() {
+        String dialogKey = "VillageMenuDialog";
+    }
+    public void pubBackGround() {
+        String dialogKey = "PubBackGround";
+    }
+    public void pubMenuDialog() {
+        String dialogKey = "PubMenuDialog";
+    }
+    public void restaurantBackGround() {
+        String dialogKey = "RestaurantBackGround";
+    }
+    public void smithBackGround() {
+        String dialogKey = "SmithBackGround";
+    }
+    public void bSmithCannotDialog() {
+        String dialogKey = "BsmithCannotDialog";
+    }
+    public void smithUpgradeMenu() {
+        String dialogKey = "SmithUpgradeMenu";
+    }
+    public void smithSwordUpgradeDialog() {
+        String dialogKey = "SmithSwordUpgradeDialog";
+    }
+    public void smithArmorUpgradeDialog() {
+        String dialogKey = "SmithArmorUpgradeDialog";
+    }
+    public void lineDialog() {
+        String dialogKey = "LineDialog";
+    }
+    public void warMenuDialog() {
+        String dialogKey = "WarMenuDialog";
+    }
+    public void mobEntranceDialog() {
+        String dialogKey = "MobEntranceDialog";
+    }
+    public void dungeonMenuDialog() {
+        String dialogKey = "DungeonMenuDialog";
+    }
+    public void enterGoblinDialog() {
+        String dialogKey = "EnterGoblinDialog";
+    }
+    public void enterOrkDialog() {
+        String dialogKey = "EnterOrkDialog";
+    }
+    public void enterNightElf() {
+        String dialogKey = "EnterNightElf";
+    }
+    public void earnGold() {
+        String dialogKey = "EarnGold";
+    }
+    public void badEnding() {
+        String dialogKey = "BadEnding";
+    }
+    public void trueEnding() {
+        String dialogKey = "TrueEnding";
 }
--- src/main/java/service/WeaponService.java
+++ src/main/java/service/WeaponService.java
@@ -8,8 +8,9 @@ import repository.WeaponRepository;
 public class WeaponService {
     private WeaponRepository weaponRepository = null;
     private PlayerStatusService playerStatusService = null;
+    DialogReadService dialogReadService = new DialogReadService();
 
-    public WeaponService() {
+    public WeaponService(PlayerStatusService playerStatusService) {
         playerStatusService = new PlayerStatusService();
         weaponRepository = new WeaponRepository();
         weaponRepository.createWeaponTable();
@@ -19,30 +20,30 @@ public class WeaponService {
     public void UpgradeSword(CharactorDto charactorDto) {
         Weapon weapon = weaponRepository.findByItemIdLv(0, charactorDto.toEntity().getSwordLv() + 1);
         if (weapon == null) {
-            DialogReadService.getDialog("BSmithCannotDialog");
+            dialogReadService.titleDialog();
         } else if (playerStatusService.GoldChange(charactorDto,weapon.getGold())) {
             CharactorDto temp = new CharactorDto(charactorDto.toEntity());
             temp.setAtk(weapon.getGear());
             temp.setSwordLv(weapon.getLv());
             playerStatusService.update(charactorDto);
-            DialogReadService.getDialog("SmithSwordUpgradeDialog");
+            dialogReadService.smithSwordUpgradeDialog();
             System.out.print(playerStatusService.findById(1L).getAtk());
-            DialogReadService.getDialog("LineDialog");
+            dialogReadService.lineDialog();
         }
     }
 
     public void UpgradeArmor(CharactorDto charactorDto) {
         Weapon weapon = weaponRepository.findByItemIdLv(1, charactorDto.toEntity().getArmorLv() + 1);
         if (weapon == null) {
-            DialogReadService.getDialog("BsmithCannotDialog");
+            dialogReadService.bSmithCannotDialog();
         } else if (playerStatusService.GoldChange(charactorDto,weapon.getGold())) {
             CharactorDto temp = new CharactorDto(charactorDto.toEntity());
             temp.setDef(weapon.getGear());
             temp.setArmorLv(weapon.getLv());
             playerStatusService.update(charactorDto);
-            DialogReadService.getDialog("SmithArmorUpgradeDialog");
+            dialogReadService.smithArmorUpgradeDialog();
             System.out.print(playerStatusService.findById(1L).getDef());
-            DialogReadService.getDialog("LineDialog");
+            dialogReadService.lineDialog();
         }
     }
 }
